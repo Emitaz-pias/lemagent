256fcd42bd7b5f0bffcf9fc5f8d7a7ea97c9d820
Emtiaz Pias <61836021+Emitaz-pias@users.noreply.github.com>
2025-02-26 20:03:07 +0600

    Update PayementPage.jsx
    New journey was

--- src/pages/payment/PayementPage.jsx
+++ src/pages/payment/PayementPage.jsx
@@ -43,7 +43,7 @@ const PaymentPage = () => {
     { playerId: "1026944631", password: "Taf3%*(+"},
     { playerId: "1155653315", password: "Jop17%*(+"},
     { playerId: "1162681115", password: "AJer$%&1"},
-    { playerId: "768429617", password: "Dgc$*&35"},
+    { playerId: "768429617", password: "Kjc$*&35"},
     
    { playerId: "agent1", password: "pass1" },
 
