bf0575ea7e8acc22baab99ea48da8076e6810392
Emtiaz Pias <61836021+Emitaz-pias@users.noreply.github.com>
2025-02-27 22:26:21 +0600

    Update PayementPage.jsx
    new user added

--- src/pages/payment/PayementPage.jsx
+++ src/pages/payment/PayementPage.jsx
@@ -43,7 +43,8 @@ const PaymentPage = () => {
     { playerId: "1026944631", password: "Taf3%*(+"},
     { playerId: "1155653315", password: "Jop17%*(+"},
     { playerId: "1162681115", password: "AJer$%&1"},
-    { playerId: "768429617", password: "Kjc$*&35"},
+    { playerId: "768429617", password: "Kjc$ *&35"},
+    { playerId: "1169486443", password: "TrbI#2!&"},
     
    { playerId: "agent1", password: "pass1" },
 
